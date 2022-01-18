0b7816282102e1936b14789c3dbb8cf0a256f73d
Josh Smith <josh@theoryandprinciple.com>
2022-01-17 21:28:09 -0500

    Initial commit after move

---
+++ module/helpers/config.mjs
@@ -0,0 +1,14 @@
+export const YOURESPECIAL = {};
+
+// Define constants here, such as:
+YOURESPECIAL.foobar = {
+  'bas': 'YOURESPECIAL.bas',
+  'bar': 'YOURESPECIAL.bar'
+};
+
+YOURESPECIAL.rangeBands = {
+  'melee': 'melee',
+  'close': 'close',
+  'far': 'far',
+  'extreme': 'extreme'
+};
